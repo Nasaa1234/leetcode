40812d11a2ffc16578a2b41bc1ecf75afcf5cc08
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-11-16 20:29:33 +0800

    [Random] 338. Counting Bits

---
+++ 338._Counting_Bits/338._Counting_Bits.java
@@ -0,0 +1,19 @@
+// =====================================
+// Problem: 338. Counting Bits
+// Date: 2025-11-16
+// Language: Java
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+public class 338._Counting_Bits {
+    public static void main(String[] args) {
+        System.out.println("Solving: 338. Counting Bits");
+    }
+}
---
+++ 338._Counting_Bits/README.md
@@ -0,0 +1,8 @@
+# LeetCode Problems
+
+## 338. Counting Bits
+- Date: 2025-11-16
+- Language: java
+- LeetCode: [338. Counting Bits](https://leetcode.com/problems/338.-counting-bits/)
+- Status: ❌ Not Solved
+
