1f5615e6e2e1e3f0199f28ba713058e054c6c0cd
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-11-16 14:44:04 +0800

    [Daily] Number of Substrings With Only 1s

---
+++ 2025-11-16/Number_of_Substrings_With_Only_1s.java
@@ -0,0 +1,19 @@
+// =====================================
+// Problem: Number of Substrings With Only 1s
+// Date: 2025-11-16
+// Language: Java
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+public class Number_of_Substrings_With_Only_1s {
+    public static void main(String[] args) {
+        System.out.println("Solving: Number of Substrings With Only 1s");
+    }
+}
---
+++ 2025-11-16/README.md
@@ -0,0 +1,8 @@
+# LeetCode Problems
+
+## Number of Substrings With Only 1s
+- Date: 2025-11-16
+- Language: java
+- LeetCode: [Number of Substrings With Only 1s](https://leetcode.com/problems/number-of-substrings-with-only-1s/)
+- Status: ❌ Not Solved
+
