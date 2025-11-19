c3a80ac41d80c7c8db5254d6228a4ed963c9c10e
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-11-19 09:57:18 +0800

    [Random] 2257. Count Unguarded Cells in the Grid

---
+++ 2257._Count_Unguarded_Cells_in_the_Grid/2257._Count_Unguarded_Cells_in_the_Grid.js
@@ -0,0 +1,21 @@
+// =====================================
+// Problem: 2257. Count Unguarded Cells in the Grid
+// Date: 2025-11-19
+// Language: JavaScript
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+
+
+function solve() {
+  console.log("Solving: 2257. Count Unguarded Cells in the Grid");
+}
+
+solve();
---
+++ 2257._Count_Unguarded_Cells_in_the_Grid/README.md
@@ -0,0 +1,8 @@
+# LeetCode Problems
+
+## 2257. Count Unguarded Cells in the Grid
+- Date: 2025-11-19
+- Language: js
+- LeetCode: [2257. Count Unguarded Cells in the Grid](https://leetcode.com/problems/2257.-count-unguarded-cells-in-the-grid/)
+- Status: ❌ Not Solved
+
