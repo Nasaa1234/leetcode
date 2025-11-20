aba4b01e4750ff0b2682a14bab0242cf2cdb0496
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-11-20 10:34:58 +0800

    [Daily] Set Intersection Size At Least Two

---
+++ 2025-11-20/README.md
@@ -0,0 +1,8 @@
+# LeetCode Problems
+
+## Set Intersection Size At Least Two
+- Date: 2025-11-20
+- Language: js
+- LeetCode: [Set Intersection Size At Least Two](https://leetcode.com/problems/set-intersection-size-at-least-two/)
+- Status: ❌ Not Solved
+
---
+++ 2025-11-20/Set_Intersection_Size_At_Least_Two.js
@@ -0,0 +1,21 @@
+// =====================================
+// Problem: Set Intersection Size At Least Two
+// Date: 2025-11-20
+// Language: JavaScript
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+
+
+function solve() {
+  console.log("Solving: Set Intersection Size At Least Two");
+}
+
+solve();
