56a1fa3c730d004b85b01b9f236bf7b8e6a675b9
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-11-20 11:22:55 +0800

    [Daily] Set Intersection Size At Least Two

--- 2025-11-20/README.md
+++ 2025-11-20/README.md
@@ -2,7 +2,7 @@
 
 ## Set Intersection Size At Least Two
 - Date: 2025-11-20
-- Language: js
+- Language: cpp
 - LeetCode: [Set Intersection Size At Least Two](https://leetcode.com/problems/set-intersection-size-at-least-two/)
 - Status: ❌ Not Solved
 
---
+++ 2025-11-20/Set_Intersection_Size_At_Least_Two.cpp
@@ -0,0 +1,21 @@
+// =====================================
+// Problem: Set Intersection Size At Least Two
+// Date: 2025-11-20
+// Language: C++
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Solving: Set Intersection Size At Least Two" << endl;
+    return 0;
+}
