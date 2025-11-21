2dadb08c7899e685392be6ecfae03443aec80de1
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-11-21 10:28:16 +0800

    [Daily] Unique Length-3 Palindromic Subsequences

---
+++ 2025-11-21/README.md
@@ -0,0 +1,8 @@
+# LeetCode Problems
+
+## Unique Length-3 Palindromic Subsequences
+- Date: 2025-11-21
+- Language: js
+- LeetCode: [Unique Length-3 Palindromic Subsequences](https://leetcode.com/problems/unique-length-3-palindromic-subsequences/)
+- Status: ❌ Not Solved
+
---
+++ 2025-11-21/Unique_Length-3_Palindromic_Subsequences.js
@@ -0,0 +1,21 @@
+// =====================================
+// Problem: Unique Length-3 Palindromic Subsequences
+// Date: 2025-11-21
+// Language: JavaScript
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+
+
+function solve() {
+  console.log("Solving: Unique Length-3 Palindromic Subsequences");
+}
+
+solve();
