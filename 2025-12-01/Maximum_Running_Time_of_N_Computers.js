bbef713035a41dbb87eb0056a9a150a72d68d23f
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-12-01 11:25:12 +0800

    [Daily] Maximum Running Time of N Computers

---
+++ 2025-12-01/Maximum_Running_Time_of_N_Computers.js
@@ -0,0 +1,21 @@
+// =====================================
+// Problem: Maximum Running Time of N Computers
+// Date: 2025-12-01
+// Language: JavaScript
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+
+
+function solve() {
+  console.log("Solving: Maximum Running Time of N Computers");
+}
+
+solve();
---
+++ 2025-12-01/README.md
@@ -0,0 +1,8 @@
+# LeetCode Problems
+
+## Maximum Running Time of N Computers
+- Date: 2025-12-01
+- Language: js
+- LeetCode: [Maximum Running Time of N Computers](https://leetcode.com/problems/maximum-running-time-of-n-computers/)
+- Status: ❌ Not Solved
+
