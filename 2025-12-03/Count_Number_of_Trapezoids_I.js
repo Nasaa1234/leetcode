f7bf8a626baef66a1594ea64e8c7f64168775c92
“Your <96171370+Nasaa1234@users.noreply.github.com>
2025-12-03 06:29:04 +0800

    [Daily] Count Number of Trapezoids I

---
+++ 2025-12-03/Count_Number_of_Trapezoids_I.js
@@ -0,0 +1,21 @@
+// =====================================
+// Problem: Count Number of Trapezoids I
+// Date: 2025-12-03
+// Language: JavaScript
+// =====================================
+
+/*
+==============================
+        DESCRIPTION
+==============================
+
+
+*/
+
+
+
+function solve() {
+  console.log("Solving: Count Number of Trapezoids I");
+}
+
+solve();
---
+++ 2025-12-03/README.md
@@ -0,0 +1,8 @@
+# LeetCode Problems
+
+## Count Number of Trapezoids I
+- Date: 2025-12-03
+- Language: js
+- LeetCode: [Count Number of Trapezoids I](https://leetcode.com/problems/count-number-of-trapezoids-i/)
+- Status: ❌ Not Solved
+
